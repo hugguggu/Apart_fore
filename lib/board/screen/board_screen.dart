9d225a6084f07d17ee23ff83105fca812fee7350
hugguggu <hugguggu@gmail.com>
2022-09-09 12:07:55 +0900

    Add Pull refresh, loding feature

--- lib/board/screen/board_screen.dart
+++ lib/board/screen/board_screen.dart
@@ -1,7 +1,10 @@
 import 'package:apart_forest/board/model/model_post.dart';
+import 'package:apart_forest/board/screen/write_screen.dart';
 import 'package:apart_forest/board/widget/posting_card.dart';
 import 'package:apart_forest/board/widget/search_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class BoardScreen extends StatefulWidget {
   final List<String> list = List.generate(10, (index) => "Text $index");
@@ -11,17 +14,14 @@ class BoardScreen extends StatefulWidget {
 
 class _BoardScreenState extends State<BoardScreen> {
   List<Post> postList = [];
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
 
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
       appBar: AppBar(
-        title: const Text('무슨무슨 아파트 게시판'),
+        title: const Text('A.Fore Board'),
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.search),
@@ -31,32 +31,84 @@ class _BoardScreenState extends State<BoardScreen> {
           ),
         ],
       ),
-      body: Center(
-          child: Expanded(
-              child: ListView.builder(
-                  itemCount: postList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return postcard(post: postList[index]);
-                  }))),
+      body: SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: true,
+        header: WaterDropHeader(),
+        footer: CustomFooter(
+          builder: (BuildContext context, LoadStatus mode) {
+            Widget body;
+            if (mode == LoadStatus.idle) {
+              body = Text("pull up load");
+            } else if (mode == LoadStatus.loading) {
+              body = CupertinoActivityIndicator();
+            } else if (mode == LoadStatus.failed) {
+              body = Text("Load Failed!Click retry!");
+            } else if (mode == LoadStatus.canLoading) {
+              body = Text("release to load more");
+            } else {
+              body = Text("No more Data");
+            }
+            return Container(
+              height: 55.0,
+              child: Center(child: body),
+            );
+          },
+        ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child: ListView.builder(
+          itemBuilder: (c, i) => postcard(post: postList[i]),
+          // itemExtent: 100.0,
+          itemCount: postList.length,
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Add your onPressed code here!
-          Post post = Post.fromMap({
-            'title': 'BCDE',
-            'bodyText':
-                'Preview_ABCDEFGHIJKLMNOPQRSTUVWXYZ_ABCDEFGHIJKLMNOPQRSTUVWXYZ_ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-            'viewer': 13,
-            'blind': false,
-            'writer': 'XYZ',
-            'visit': 12
-            // 'comment' : ['abc', 'dec']
-          });
-          postList.add(post);
-          setState(() {});
+          // post_test();
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => WriteScreen()),
+          );
         },
         backgroundColor: Colors.blueAccent,
         child: const Icon(Icons.add),
       ),
     ));
+  }
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    // items.add((items.length + 1).toString());
+    post_test();
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
+  }
+
+  // ignore: non_constant_identifier_names
+  void post_test() {
+    Post post = Post.fromMap({
+      'title': 'BCDE',
+      'bodyText':
+          'Preview_ABCDEFGHIJKLMNOPQRSTUVWXYZ_ABCDEFGHIJKLMNOPQRSTUVWXYZ_ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      'viewer': 13,
+      'blind': false,
+      'writer': 'XYZ',
+      'visit': 12
+      // 'comment' : ['abc', 'dec']
+    });
+    postList.add(post);
+    setState(() {});
   }
 }
