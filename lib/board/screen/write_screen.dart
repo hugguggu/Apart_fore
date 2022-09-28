f983899d70c21e4fa38142c778f084a4171e8845
hugguggu <hugguggu@gmail.com>
2022-09-28 23:17:24 +0900

    글쓰기 팝업 알람 기능 추가

--- lib/board/screen/write_screen.dart
+++ lib/board/screen/write_screen.dart
@@ -1,5 +1,6 @@
 import 'package:apart_forest/board/model/network_singleton.dart';
 import 'package:apart_forest/board/model/post_item_singlton.dart';
+import 'package:apart_forest/board/screen/board_screen.dart';
 import 'package:flutter/material.dart';
 
 class WriteScreen extends StatefulWidget {
@@ -16,7 +17,6 @@ class WriteScreen extends StatefulWidget {
 }
 
 class _WriteScreenState extends State<WriteScreen> {
-  // List<String> dropdownList = ['궁금해요', '불편해요', '칭찬해요', '나눔해요', '공유해요'];
   int selectedDropdown = 0;
   final titleTextFieldController = TextEditingController();
   final contentTextFieldController = TextEditingController();
@@ -35,7 +35,14 @@ class _WriteScreenState extends State<WriteScreen> {
           title: const Text('새 글쓰기'),
           leading: IconButton(
             onPressed: () {
-              Navigator.pop(context);
+              if (selectedDropdown == 0 &&
+                  titleTextFieldController.text.isEmpty &&
+                  contentTextFieldController.text.isEmpty) {
+                Navigator.pop(context);
+              } else {
+                _showdBackDialog(context)
+                    .then((value) => (value) ? Navigator.pop(context) : null);
+              }
             },
             icon: const Icon(Icons.backspace),
           ),
@@ -43,15 +50,19 @@ class _WriteScreenState extends State<WriteScreen> {
             MaterialButton(
               // textColor: Colors.white,
               onPressed: () {
-                NetworkSingleton().posting(
-                    selectedDropdown,
-                    titleTextFieldController.text,
-                    contentTextFieldController.text);
-                PostItem().addLoadPostItem();
-                Navigator.pop(context);
+                if (selectedDropdown == 0) {
+                  _showdTextFieldDialog(context, 0);
+                } else if (titleTextFieldController.text.isEmpty) {
+                  _showdTextFieldDialog(context, 1);
+                } else if (contentTextFieldController.text.isEmpty) {
+                  _showdTextFieldDialog(context, 2);
+                } else {
+                  _showdSaveDialog(context)
+                      .then((value) => (value) ? Navigator.pop(context) : null);
+                }
               },
               child: const Text(
-                "Save",
+                "저장",
                 style: TextStyle(fontSize: 16, color: Colors.redAccent),
               ),
               // shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
@@ -102,5 +113,85 @@ class _WriteScreenState extends State<WriteScreen> {
             ),
           ],
         ));
+  }
+
+  Future<dynamic> _showdSaveDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        // title: Text('글쓰기'),
+        content: const Text('작성한 글을 게시하겠습니까?'),
+        actions: [
+          ElevatedButton(
+            child: const Text('확인'),
+            onPressed: () {
+              NetworkSingleton().posting(
+                  selectedDropdown,
+                  titleTextFieldController.text,
+                  contentTextFieldController.text);
+              PostItem().addLoadPostItem();
+
+              Navigator.pop(context, true);
+            },
+          ),
+          ElevatedButton(
+            child: const Text('취소'),
+            onPressed: () {
+              Navigator.pop(context, false);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> _showdTextFieldDialog(BuildContext context, int code) {
+    String txt;
+    if (code == 0) {
+      txt = '카테고리를 지정해 주세요';
+    } else if (code == 1) {
+      txt = '글의 제목을 작성해 주세요';
+    } else if (code == 2) {
+      txt = '글의 내용을 작성해 주세요';
+    } else {
+      txt = '알수없는 오류';
+    }
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        content: Text(txt),
+        actions: [
+          ElevatedButton(
+            child: const Text('확인'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> _showdBackDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        content: const Text('작성중이던 내용이 있습니다.\n취소하시겠습니까?'),
+        actions: [
+          ElevatedButton(
+            child: const Text('확인'),
+            onPressed: () {
+              Navigator.pop(context, true);
+            },
+          ),
+          ElevatedButton(
+            child: const Text('취소'),
+            onPressed: () {
+              Navigator.pop(context, false);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
